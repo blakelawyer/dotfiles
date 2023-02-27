2228233c270d668e4d8814a1a8f3f3ebddbbda13
Blake Lawyer <blakethelawyer@gmail.com>
2023-02-27 02:57:22 +0000

    dotfiles consolidated

--- install.sh
+++ install.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-mkdir -p "$HOME/.config/nvim"
-mkdir -p "$HOME/.config/nvim/undo"
-ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
-rm -rf "$HOME/.config/X11"
-ln -s "$HOME/dotfiles/X11" "$HOME/.config"
-rm -rf "$HOME/.config/i3"
-ln -s "$HOME/dotfiles/i3" "$HOME/.config"
+# nvim
+rm -rf "$HOME/.config/nvim"
+ln -s "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
 
+# i3
+rm -rf "$HOME/.config/i3"
+ln -s "$HOME/dotfiles/i3" "$HOME/.config/i3"
+
+# wezterm
+rm -rf "$HOME/.config/wezterm"
+ln -s "$HOME/dotfiles/wezterm" "$HOME/.config/wezterm"
+
+# zsh
+rm -rf "$HOME/.zshrc"
+ln -s "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
+
+# tmux
+rm -rf "$HOME/.tmux.conf"
+ln -s "$HOME/dotfiles/.tmux.conf" "$HOME/.tmux.conf"
+
+# newsboat
+rm -rf "$HOME/.newsboat"
+ln -s "$HOME/dotfiles/.newsboat" "$HOME/.newsboat"
+
+# X11
+rm -rf "$HOME/.xinitrc"
+ln -s "$HOME/dotfiles/.xinitrc" "$HOME/.xinitrc"
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require 'wezterm'
+
+return {
+  font_size = 15,
+  font = wezterm.font 'Hack',
+  color_scheme = "GruvboxDarkHard",
+}
