8974aef102f7598b6fe48019c7bc8b6d971bc333
Blake Lawyer <blakethelawyer@gmail.com>
2023-04-06 16:50:48 -0500

    chatgpt api plugin

--- nvim/lua/blake/core/colorscheme.lua
+++ nvim/lua/blake/core/colorscheme.lua
@@ -1,5 +1,5 @@
 vim.g["everforest_background"] = "hard"
-local status, _ = pcall(vim.cmd, "colorscheme everforest")
+local status, _ = pcall(vim.cmd, "colorscheme gruvbox")
 if not status then
 	print("Colorscheme not found!")
 	return
