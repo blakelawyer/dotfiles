cacda4e08124a489fa43a987435bdbc5365ab407
Blake Lawyer <blakethelawyer@gmail.com>
2023-05-10 13:31:15 -0500

    remove old copilot plugin

--- nvim/lua/blake/plugins/nvim-cmp.lua
+++ nvim/lua/blake/plugins/nvim-cmp.lua
@@ -20,7 +20,6 @@ end
 require("luasnip/loaders/from_vscode").lazy_load()
 
 vim.opt.completeopt = "menu,menuone,noselect"
-vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
 
 cmp.setup({
 	snippet = {
@@ -39,19 +38,16 @@ cmp.setup({
 	}),
 	-- sources for autocompletion
 	sources = cmp.config.sources({
-		{ name = "copilot" }, -- copilot
 		{ name = "nvim_lsp" }, -- lsp
 		{ name = "luasnip" }, -- snippets
 		{ name = "buffer" }, -- text within current buffer
 		{ name = "path" }, -- file system paths
 	}),
-	-- vim.api.nvim_set_h1(0, "CmpItemKindCopilot", {fg = "#6CC644" })
 	-- configure lspkind for vs-code like icons
 	formatting = {
 		format = lspkind.cmp_format({
 			maxwidth = 50,
 			ellipsis_char = "...",
-			symbol_map = { Copilot = " " },
 		}),
 	},
 })
