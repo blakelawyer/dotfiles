408fcd23f627a1caafcc68e3c3e7274f60b29131
Blake Lawyer <blakethelawyer@gmail.com>
2023-07-06 16:42:21 -0500

    alacritty, xinitrc, xModmap

--- install.sh
+++ install.sh
@@ -9,6 +9,21 @@ rm -rf "$HOME/.tmux.conf"
 ln -s "$HOME/dotfiles/.tmux.conf" "$HOME/.tmux.conf"
 
 # zsh
-# rm -rf "$HOME/.zshrc"
-# ln -s "$HOME/dotfiles/.zshrc" "$HOME/.config/zsh/.zshrc"
+rm -rf "$HOME/.zshrc"
+ln -s "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
 
+# i3
+rm -rf "$HOME/.config/i3"
+ln -s "$HOME/dotfiles/.config/i3" "$HOME/.config/i3"
+
+# alacritty
+rm -rf "$HOME/.config/alacritty"
+ln -s "$HOME/dotfiles/.config/alacritty" "$HOME/.config/alacritty"
+
+# xinitrc
+rm -rf "$HOME/.xinitrc"
+ln -s "$HOME/dotfiles/.xinitrc" "$HOME/.xinitrc"
+
+# Xmodmap
+rm -rf "$HOME/.Xmodmap"
+ln -s "$HOME/dotfiles/.Xmodmap" "$HOME/.Xmodmap"
