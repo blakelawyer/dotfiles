b29a8d3b3a688a73665133ab8a0b491be20b4d0b
Blake Lawyer <blakethelawyer@gmail.com>
2023-09-17 18:30:30 -0500

    refactored colorschemes

--- nvim/lua/blake/lazy.lua
+++ nvim/lua/blake/lazy.lua
@@ -11,4 +11,4 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "blake.plugins" }, { import = "blake.plugins.lsp" } }, {})
+require("lazy").setup({ { import = "blake.plugins" }, { import = "blake.plugins.lsp" }, { import = "blake.plugins.themes" } }, {})
