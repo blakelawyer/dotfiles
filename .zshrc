9a41e42a1dfac1a774a5d63675002948cb37a1bb
Blake Lawyer <blakethelawyer@gmail.com>
2026-01-23 17:56:18 -0700

    writing setup

--- .zshrc
+++ .zshrc
@@ -17,6 +17,7 @@ alias dotfiles="cd ~/dotfiles"
 alias scripts="cd ~/dotfiles/scripts"
 alias archive="cd ~/notes/archive"
 alias notes='nvim -p ~/notes/notes.md'
+alias journal='nvim ~/notes/journal.md -c "ZenMode"'
 
 # pyenv configuration
 export PATH="$HOME/.pyenv/bin:$PATH"
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -17,36 +17,41 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({
+
+            vim.lsp.config('lua_ls', {
                 capabilities = capabilities
             })
-            lspconfig.pyright.setup({
+
+            vim.lsp.config('pyright', {
                 capabilities = capabilities
             })
-            lspconfig.texlab.setup({
-              capabilities = capabilities,
-              settings = {
-                texlab = {
-                  build = {
-                    executable = "latexmk",
-                    args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
-                    onSave = true,
-                    forwardSearchAfter = true,
-                  },
-                  forwardSearch = {
-                    executable = "zathura",
-                    args = { "--synctex-editor-command", "nvr --remote-silent +%{line} %{input}", "%p" },
-                  },
-                  chktex = {
-                    onOpenAndSave = true,
-                  },
-                  completion = {
-                    disableSnippet = false,  -- Enable snippet support if desired
-                  },
+
+            vim.lsp.config('texlab', {
+                capabilities = capabilities,
+                settings = {
+                    texlab = {
+                        build = {
+                            executable = "latexmk",
+                            args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
+                            onSave = true,
+                            forwardSearchAfter = true,
+                        },
+                        forwardSearch = {
+                            executable = "zathura",
+                            args = { "--synctex-editor-command", "nvr --remote-silent +%{line} %{input}", "%p" },
+                        },
+                        chktex = {
+                            onOpenAndSave = true,
+                        },
+                        completion = {
+                            disableSnippet = false,
+                        },
+                    },
                 },
-              },
             })
+
+            vim.lsp.enable({ 'lua_ls', 'pyright', 'texlab' })
+
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
